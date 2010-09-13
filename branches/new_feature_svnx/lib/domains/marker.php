aa06d023ff5f8a80a9ebe8394f90de7ab5f1c242
sam.sjk <sam.sjk@7fd1a3b4-8816-11de-bd5f-d5dc12eeefd8>
2010-09-13 23:36:06 +0000

    Create branch new_feature_svnx

---
+++ branches/new_feature_svnx/lib/domains/marker.php
@@ -0,0 +1,33 @@
+<?php
+class marker extends Domain {
+    // define the attributes of this domain and associated filters
+
+    public function  __construct() {
+        parent::__construct();
+        $this->config = array(
+            'filters' => array(
+                'interest_id'  => array('filter'=>FILTER_VALIDATE_INT),
+                'coordinate_label' => array('filter'=>FILTER_SANITIZE_STRING),
+                'coordinate_description' => array('filter'=>FILTER_SANITIZE_STRING),
+                'marker_size' => array('filter'=>FILTER_SANITIZE_STRING),
+                'marker_color' => array('filter'=>FILTER_SANITIZE_STRING),
+                'coordinate_x'   => array('filter'=>FILTER_VALIDATE_INT),
+                'coordinate_y'   => array('filter'=>FILTER_VALIDATE_INT),
+                'bound_north'   => array('filter'=>FILTER_VALIDATE_INT),
+                'bound_south'   => array('filter'=>FILTER_VALIDATE_INT),
+                'bound_west'   => array('filter'=>FILTER_VALIDATE_INT),
+                'bound_east'   => array('filter'=>FILTER_VALIDATE_INT)
+            ),
+            'input_sources' => array(
+                'get' => INPUT_GET,
+                'add' => INPUT_GET,
+                'edit' => INPUT_GET,
+                'delete' => INPUT_GET
+            ),
+	    'relations' => array(
+		'has_many' => array(),
+		'belongs_to' => array('interest')
+	    )
+        );
+    }
+}
